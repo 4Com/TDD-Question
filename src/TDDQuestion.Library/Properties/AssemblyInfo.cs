edfa48c27c037ebb790836f25f479843569fab7d
Matt Ridgway <matt.ridgway@4com.co.uk>
2017-04-26 16:16:30 +0100

    Added projects

---
+++ src/TDDQuestion.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDQuestion.Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("4Com PLC")]
+[assembly: AssemblyProduct("TDD Interview Question")]
+[assembly: AssemblyCopyright("Copyright © 4Com PLC 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1e23a080-221e-4e46-bfec-11e5727cf246")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/TDDQuestion.Library.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDQuestion.Library.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("4Com PLC")]
+[assembly: AssemblyProduct("TDD Interview Question")]
+[assembly: AssemblyCopyright("Copyright © 4Com PLC 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("59ebb1d9-9c7a-4065-ae5f-e94441f86006")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
