4d88faacef05532cc1788f96800f25cefc1f388a
Matt Ridgway <matt.ridgway@4com.co.uk>
2017-04-26 16:25:30 +0100

    Added tests and enough for the code to compile

---
+++ src/TDDQuestion.Library/LetterToNumberConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDQuestion.Library
+{
+	internal class LetterToNumberConverter
+	{
+		public int Convert(string letters)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ test/TDDQuestion.Library.Tests/LetterToNumberHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TDDQuestion.Library.Tests
+{
+	public class LetterToNumberHelperTests
+	{
+		[Theory(DisplayName = "GIVEN a letter WHEN calling convert THEN return the correct number")]
+		[InlineData(null, 0)]
+		[InlineData("", 0)]
+		[InlineData(" ", 0)]
+		[InlineData("  ", 0)]
+		[InlineData("A", 1)]
+		[InlineData("B", 2)]
+		[InlineData("Z", 26)]
+		[InlineData("AA", 27)]
+		[InlineData("AB", 28)]
+		[InlineData("BA", 53)]
+		[InlineData("ZZ", 702)]
+		[InlineData("AAA", 703)]
+		public void ConvertLetterToNumber(string letter, int expectedValue)
+		{
+			var converter = new LetterToNumberConverter();
+			var answer = converter.Convert(letter);
+			answer.Should().Be(expectedValue);
+		}
+	}
+}
